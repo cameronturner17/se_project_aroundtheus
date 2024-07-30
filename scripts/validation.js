e254de24a22752628836b1fadf50149342e56591
Cameron Turenr <camerontturner1@gmail.com>
2024-07-30 15:41:29 -0500

    added span

--- scripts/validation.js
+++ scripts/validation.js
@@ -65,7 +65,7 @@ const config = {
     submitButtonSelector: ".modal__button",
     inactiveButtonClass: "modal__button_disabled",
     inputErrorClass: "modal__input_type_error",
-    errorClass: "modal__error"   
+    errorClass: "modal__error_visible"   
 };
 
 enableValidation(config);
